4110875308783d58a8a58c12628fac809be3233c
SirojiddinBoltayev <sirojboltayev0948@gmail.com>
2022-06-17 00:10:23 +0500

    keyboard muammosi hal bo'ldi

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,148 +1,179 @@
 import 'package:flutter/material.dart';
+// import 'package:food_shop/model/login_data.dart';
+import 'package:food_shop/sign_up_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Column(
+  State<LoginPage> createState() => _LoginPageState();
+}
 
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  flex: 4,
-                  child: Container(
-                    // height: 147,
-                    width: double.infinity,
-                    child: Align(
-                        child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          "assets/images/burger 1.png",
-                          scale: 2,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 38.0),
-                          child: Text(
-                            'NeedFood',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 32,
-                                fontWeight: FontWeight.bold),
+// class LoginData{
+//   String login;
+//   String password;
+//   int id;
+//   LoginData({required this.id, required this.login,required this.password});
+//
+//   LoginData({login: "", password: "", id: 0});
+//
+// }
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        width: screenSize,
+        height: screenHeight,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/bg-image.png"),
+          ),
+        ),
+        child: Stack(
+          // mainAxisSize: MainAxisSize.min,
+          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 147,
+              width: double.infinity,
+              child: Align(
+                  child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/images/burger 1.png",
+                    scale: 2,
+                  ),
+                  const Padding(
+                    padding: const EdgeInsets.only(top: 38.0),
+                    child: Text(
+                      'NeedFood',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              )),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/Rectangle 1.png"),
+                ),
+              ),
+            ),
+            SizedBox(
+              // height: 10,
+              width: double.infinity,
+              child: Align(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 150),
+                      Text(
+                        "Login",
+                        style: TextStyle(
+                            fontSize: 28, fontWeight: FontWeight.bold),
+                      ),
+                      Container(
+                        height: 80,
+                        padding:
+                            const EdgeInsets.only(top: 35, left: 20, right: 20),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Login",
                           ),
                         ),
-                      ],
-                    )),
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/Rectangle 1.png"),
                       ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 11,
-                  child: Container(
-                    // height: 147,
-                    width: double.infinity,
-                    child: Align(
-                        child: ListView(
-                      children: [
-                        Column(
+                      Container(
+                        height: 60,
+                        padding: const EdgeInsets.only(
+                          top: 15,
+                          left: 20,
+                          right: 20,
+                        ),
+                        child: const TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Enter your password",
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Fogot password ?",
+                            style: TextStyle(color: Colors.grey, fontSize: 16),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 150,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text("Login"),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 140,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.end,
                           children: [
-                            Padding(
-                              padding: EdgeInsets.only(top: 68),
+                            const Center(
                               child: Text(
-                                "Login",
-                                style: TextStyle(
-                                    fontSize: 28, fontWeight: FontWeight.bold),
+                                "No account yet?",
+                                style:
+                                    TextStyle(color: Colors.grey, fontSize: 14),
                               ),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  top: 20, left: 20, right: 20),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  labelText: "Login",
+                            Center(
+                              child: InkWell(
+                                onTap: () {
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (BuildContext context) =>
+                                              SignUpPage()));
+                                },
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: const [
+                                    Text("Sign up now",
+                                        style: TextStyle(
+                                            color: Colors.blue,
+                                            fontWeight: FontWeight.w500)),
+                                    Icon(
+                                      Icons.keyboard_arrow_right_outlined,
+                                      color: Colors.blue,
+                                      size: 16,
+                                    )
+                                  ],
                                 ),
                               ),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  top: 15, left: 20, right: 20),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  labelText: "Enter your password",
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(top: 10),
-                              child: TextButton(
-                                onPressed: () {},
-                                child: Text(
-                                  "Fogot password ?",
-                                  style:
-                                      TextStyle(color: Colors.grey, fontSize: 16),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ],
-                    )),
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/bg-image.png"),
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: GridView.count(
-                    
-                    crossAxisCount: 2,
-                    children: [
-                      Center(
-                        child: Text(
-                          "No account yet?",
-                          style: TextStyle(color: Colors.grey, fontSize: 14),
-                        ),
-                      ),
-                      Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text("Sign up now",
-                                style: TextStyle(
-                                    color: Colors.blue,
-                                    fontWeight: FontWeight.w500)),
-                            Icon(
-                              Icons.keyboard_arrow_right_outlined,
-                              color: Colors.blue,
-                              size: 16,
-                            )
+                            // SizedBox(
+                            //   height: 8,
+                            // ),
                           ],
                         ),
                       ),
-                      // SizedBox(
-                      //   height: 8,
-                      // ),
                     ],
                   ),
-                )
-              ],
+                ),
+              ),
             ),
-
+          ],
         ),
-     
+      ),
     );
   }
 }
---
+++ lib/sign_up_page.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:food_shop/login_page.dart';
+
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        width: screenSize,
+        height: screenHeight,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/bg-image.png"),
+          ),
+        ),
+        child: Stack(
+          // mainAxisSize: MainAxisSize.min,
+          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 147,
+              width: double.infinity,
+              child: Align(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/images/burger 1.png",
+                        scale: 2,
+                      ),
+                      const Padding(
+                        padding: const EdgeInsets.only(top: 38.0),
+                        child: Text(
+                          'NeedFood',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  )),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/Rectangle 1.png"),
+                ),
+              ),
+            ),
+            SizedBox(
+              // height: 10,
+              width: double.infinity,
+              child: Align(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 150),
+                      Text(
+                        "Sign up",
+                        style: TextStyle(
+                            fontSize: 28, fontWeight: FontWeight.bold),
+                      ),
+                      Container(
+                        height: 80,
+                        padding:
+                        const EdgeInsets.only(top: 35, left: 20, right: 20),
+                        child: const TextField(
+                          // cursorRadius:Radius.circular(10) ,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+
+                            labelText: "Login",
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 60,
+                        padding: const EdgeInsets.only(
+                          top: 15,
+                          left: 20,
+                          right: 20,
+                        ),
+                        child: const TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Enter your password",
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30,),
+                      SizedBox(
+                        width: 150,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text("Sign up"),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 140,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            const Center(
+                              child: Text(
+                                "You have account",
+                                style:
+                                TextStyle(color: Colors.grey, fontSize: 14),
+                              ),
+                            ),
+                            Center(
+                              child: InkWell(
+                                onTap: (){
+                                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context)=>LoginPage()));
+
+                                },
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: const [
+                                    Text("Login",
+                                        style: TextStyle(
+                                            color: Colors.blue,
+                                            fontWeight: FontWeight.w500)),
+                                    Icon(
+                                      Icons.keyboard_arrow_right_outlined,
+                                      color: Colors.blue,
+                                      size: 16,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            // SizedBox(
+                            //   height: 8,
+                            // ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
